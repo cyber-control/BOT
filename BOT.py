1f834000205435eaff917b64920e345d44196eb9
Daniel <74275414+cyber-control@users.noreply.github.com>
2020-11-11 13:25:09 -0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,5 +1,7 @@
 #encoding: UTF-8
 import sys
+import actions 
+import logins
 from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
@@ -30,71 +32,13 @@ elif (head == '--head'):
 
 print('BOT configurado.')
 
-def gni(firefox):
-	sleep(5)
-	acessar = firefox.find_element_by_link_text('Acessar Perfil')
-	acessar.click()
-	
-	sleep(6)
-
-	firefox.switch_to_window(firefox.window_handles[-1])
-	
-	print('um novo ciclo iniciou')
-
-	try:
-		try:
-			obj_1 = firefox.find_element_by_xpath('//span/span[1]/button')
-			obj_1.click()
-		except:
-			obj = firefox.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/button')
-			obj.click()
-
-		sleep(3)
-
-		firefox.close()
-		firefox.switch_to_window(firefox.window_handles[0])
-
-		sleep(1)
-
-		confirmar = firefox.find_element_by_id('btn-confirmar')
-		confirmar.click()
-
-		print('ciclo concluido com sucesso')
-	except:
-		print('erro ao concluir o ciclo: verifique sua conexao com a internet e tente de novo.')
-
-def face_login(user,password,firefox):
-	firefox.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[5]/button/span[2]').click()
-	firefox.find_element_by_id('email').send_keys(user)
-	firefox.find_element_by_id('pass').send_keys(password)
-	firefox.find_element_by_id('loginbutton').send_keys(Keys.ENTER)
-
-def insta_login(user,password,firefox):
-	firefox.find_element_by_name('username').send_keys(user)
-	firefox.find_element_by_name('password').send_keys(password)
-	firefox.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button').send_keys(Keys.ENTER)
-
-def gni_login(n_user,n_password,firefox):
-	email = firefox.find_element_by_name("email")
-	senha = firefox.find_element_by_name("senha")
-	email.send_keys(n_user)
-	senha.send_keys(n_password)
-	senha.send_keys(Keys.ENTER)
-
-	sleep(5)
-	firefox.get("https://www.ganharnoinsta.com/painel/?pagina=sistema")  
-	sleep(2)
-
-	button = firefox.find_element_by_id("btn_iniciar")
-	button.click()
-
-#o bloco de codigo a seguir é o bloco de verificação de parametros e execução de funções pensando em expansão no futuro (ainda em trabalho)
+#o bloco de codigo a seguir é o bloco de verificação de parametros e execução de funções pensando em expansão no futuro 
 if (options_0 == '-f'):
 	try:
 		firefox.get('https://www.instagram.com/')
 		print('logando no instagram')
 		sleep(6)
-		face_login(user,password,firefox)
+		logins.face_login(user,password,firefox)
 		print('logado com sucesso no instagram \naguarde...')
 	except:
 		print('erro ao logar no instagram: verifique as credenciais ou conexao com a internet e tente de novo')
@@ -104,7 +48,7 @@ elif (options_0 == '-i'):
 		firefox.get('https://www.instagram.com/')
 		print('logando no instagram')
 		sleep(6)
-		insta_login(user,password,firefox)
+		logins.insta_login(user,password,firefox)
 		print('logado com sucesso no instagram \naguarde...')
 	except:
 		print('erro ao logar no instagram: verifique as credenciais ou conexao com a internet e tente de novo')	
@@ -115,19 +59,23 @@ else:
 
 sleep(15)
 
-if (options_1 != comand_list[0]):
+if(options_1 == comand_list[0]):
+	try:
+		print('acessando o site:'+link_list[0])
+
+		firefox.get(link_list[0])
+		logins.gni_login(n_user,n_password,firefox)
+
+		print('logado com sucesso no site: '+link_list[0])
+	except:
+		print('erro ao logar no site:'+link_list[0]+'verifique susa credenciais e conexao com a internet')
+
+	try:
+		print('iniciando o loop de ciclos')
+		while(True):
+			actions.gni(firefox)
+	except:
+		print('programa encerrado')
+else:
 	print('parametro:'+options_1+' não reconhecido')
 	quit()
-else:
-	print('acessando o site:'+link_list[0])
-	firefox.get(link_list[0])
-	gni_login(n_user,n_password,firefox)
-	print('logado com sucesso no site: '+link_list[0])
-#fim do bloco de verificação
-
-try:
-	print('iniciando o loop de ciclos')
-	while(True):
-		gni(firefox)
-except:
-	print('programa encerrado')
